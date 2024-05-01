6de125e0dec76b0684dd9ec13161fac4a2cb1e35
Max BB <max.bb@revitt.consulting>
2024-05-01 18:02:16 +0100

    update time

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const timeNow = document.getElementById("now")
 const dinner = document.getElementById("dinner")
 const dinnerTime = new Date("2024-05-01T19:00:00")
 const now = new Date()
-
+postscript = document.getElementById("postscript")
 
 
 
@@ -13,7 +13,10 @@ const hours = Math.floor(timeLeft / 1000 / 60 /60)
 const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
 
 const getHourWord = (hours) => {
-    if (hours > 1) {
+    if (hours > 1 || hours === 0) {
+        return "hours"
+    }
+    if (dinnerTime < now && hours === -1) {
         return "hours"
     }
     return "hour"
@@ -45,7 +48,15 @@ if (dinnerTime > now) {
     dinner.innerHTML = dinnerTime
 }
 
+// if (dinnerTime === now) {
+//     const title = 
+// }
+
 if (dinnerTime < now) {
+    
+
+
     titleWord.innerHTML = `since`
-    text.innerHTML = `${hours-1} ${getHourWord(hours)} and ${minutes} ${getMinWord(minutes)}`
+    text.innerHTML = `${Math.abs(hours+1)} ${getHourWord(hours)} and ${Math.abs(minutes)} ${getMinWord(minutes)}`
+    postscript.innerHTML = ` began?`
 }
